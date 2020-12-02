161fb586d3e4e4dd3b5ef451f0e482a1af471d4a
Ryan John Vickramasinghe <rvickram@uwo.ca>
2020-12-02 15:39:10 -0500

    Added Course model.

---
+++ models/Course.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = mongoose.Schema({
+    catalog_nbr : String,
+    subject : String,
+    className : String,
+    courseInfo : [{
+        class_nbr: Number,
+        start_time: String,
+        end_time: String,
+        campus: String,
+        facility_ID: String,
+        days: [],
+        instructors: [],
+        class_section: Number,
+        ssr_component: String,
+        enrl_stat: String,
+        descr: String
+    }],
+    catalog_description: String
+});
+
+module.exports = mongoose.model('Courses', CourseSchema);
